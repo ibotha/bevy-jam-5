571c5d1161b4eaa32483bf41087e0849db7c480e
Julian <julian.w16@gmail.com>
2024-07-20 22:01:45 +0200

    feat: WeatherManiac System

--- src/screen/mod.rs
+++ src/screen/mod.rs
@@ -5,6 +5,7 @@ mod loading;
 mod playing;
 mod splash;
 mod title;
+mod weather_maniac;
 
 use bevy::prelude::*;
 
@@ -18,6 +19,7 @@ pub(super) fn plugin(app: &mut App) {
         title::plugin,
         credits::plugin,
         playing::plugin,
+        weather_maniac::plugin,
     ));
 }
 
@@ -30,4 +32,5 @@ pub enum Screen {
     Title,
     Credits,
     Playing,
+    WeatherManiac,
 }
